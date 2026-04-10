f39b3aa5f7ac52fd6c3ba101fc33664633c18f76
Sevinch <ysevinchoy2197@gmail.com>
2026-04-10 16:35:52 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+
+#5-misol
+roy = [3, 6, 9, 12]
+print(roy)
+
+res = list(map(lambda x: x ** 2, roy))
+print(res)
+
+#6-misol
+roy = ["Ali", "Vali", "Hasan"]
+print(roy)
+
+res = list(map(lambda  x: x + 'Mr.', roy))
+print(res)
+
+#7-misol
+roy = [100, 200, 300]
+print(roy)
+
+res = list(map(lambda  x: str(x), roy))
+print(res)
+
+#8-misol
+roy = ["1", "2", "3", "4"]
+print(roy)
+
+res = list(map(lambda x: int(x), roy))
+print(res)
+
+#9-misol
+roy = [5, 10, 15, 20]
+print(roy)
+
+res = list(map(lambda x: x / 3, roy))
+print(res)
+
+#10-misol
+roy = ["hello", "world"]
+print(roy)
